6af06be63528bae78b977fc3f7277af71ecd1924
[Sam-Algorism] <samuel.olanrewaju@algorismng.com>
2016-12-28 18:08:38 +0100

    added like and dislike

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,6 @@
 class PicsController < ApplicationController
-	before_action :find_pic, only: [:show, :edit, :update, :destroy ]
+	before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@pics= Pic.all.order("created_at DESC")
@@ -29,6 +30,17 @@ class PicsController < ApplicationController
      @pic.destroy
      redirect_to root_path
 	end
+	def upvote
+
+		@pic.upvote_by current_user
+		redirect_to :back
+	end
+	def downvote
+
+		@pic.downvote_by current_user
+		redirect_to :back
+	end
+
 
 	def update
 		if @pic.update(pic_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pics
+  resources :pics do
+  	member do
+  		put "like", to: "pics#upvote"
+  	end
+  	member do
+  		put "dislike", to: "pics#downvote"
+  	end
+  end
   root 'pics#index'
 end
